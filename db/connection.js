de51b23975870335eec7217c9fe6c368eff8b9de
William Stock <grim118@hotmail.com>
2022-04-25 21:03:58 -0500

    added tracker database with schema tables and seeds for the tables

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MY MySQL username
+        user: 'root',
+        // MY SQL PASSWORD
+        password: 'Crankybabycasey18@',
+        database: 'tracker'
+    },
+    console.log('Connected to the tracker database.')
+);
+
+module.exports = db;
